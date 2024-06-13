c884b23e4d2a1bb54da5836e942be559dde2c42c
Micha <74900539+michafvdw@users.noreply.github.com>
2024-06-13 19:17:19 +0200

    Update esp8266_thingspeak.js

--- esp8266_thingspeak.js
+++ esp8266_thingspeak.js
@@ -1,34 +1,26 @@
-ESP8266ThingSpeak.connectWifi(
+basic.showIcon(IconNames.No)
+grove.setupWifi(
 SerialPin.P16,
 SerialPin.P2,
 BaudRate.BaudRate115200,
-"enter wifi SSI",
-"enter wifi password"
+"WIFI SSI",
+"WIFI PASSWORD"
 )
+if (grove.wifiOK()) {
+    basic.showIcon(IconNames.Yes)
+}
 basic.forever(function () {
-    if (ESP8266ThingSpeak.isWifiConnected()) {
-        if (ESP8266ThingSpeak.isLastUploadSuccessful()) {
-            basic.showString("Hello!")
-        } else {
-            basic.showString("No upload")
-        }
-        // Verbinding maken met ThingSpeak en gegevens versturen
-        ESP8266ThingSpeak.connectThingSpeak(
-        "api.thingspeak.com",
-        "enter API write key",
-        input.temperature(),
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-        )
-        // Wachten voordat je opnieuw gegevens verzendt
-        // 20 seconden wachten (meer dan de aanbevolen 15 seconden)
-        basic.pause(20000)
-    } else {
-        basic.showString("No wifi")
-    }
+    grove.sendToThinkSpeak(
+    "API KEY",
+    input.temperature(),
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+    )
+    basic.pause(2000)
 })
+
